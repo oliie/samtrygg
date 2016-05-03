1618df7a362554f73161122e6adeba0ab8587478
oliie <oliver.praesto@gmail.com>
2016-05-03 22:40:51 +0200

    Added .kit-files to its own source folder

---
+++ Gulpfile.js
@@ -0,0 +1,36 @@
+(function initGulpFile() {
+    'use strict';
+
+    var gulp    = require('gulp'),
+        kit     = require('gulp-kit'),
+        sass    = require('gulp-sass'),
+        paths   = {
+            kit:  './kit/*.kit',
+            sass: './scss/*.scss'
+        };
+
+    gulp.task('kit', function initKit() {
+        gulp
+            .src( paths.kit )
+            .pipe(kit())
+            .pipe(gulp.dest('./'));
+    });
+
+    gulp.task('sass', function initSass() {
+        gulp
+            .src( paths.sass )
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('./css'));
+    });
+
+    gulp.task('watch', function initWatch() {
+        gulp.watch( paths.kit, [ 'kit' ] );
+        gulp.watch( paths.sass, [ 'sass' ] );
+    });
+
+    gulp.task('default', [
+        'kit',
+        'sass',
+        'watch'
+    ]);
+})();
